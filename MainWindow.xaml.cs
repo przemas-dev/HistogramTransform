63f391016edca034666a3f1bfc95fd980d88d10c
Przemysław Reszka <przemyslawreszka1@gmail.com>
2023-01-16 08:57:40 +0100

    update gitignore, add opening and saving image

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,42 @@ namespace HistogramTransform
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private BitmapImage _bitmapImage;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog();
+            openDialog.Title = "Wybierz obraz";
+            openDialog.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|Portable Network Graphic (*.png)|*.png";
+            if (openDialog.ShowDialog() == true)
+            {
+
+                _bitmapImage = new BitmapImage(new Uri(openDialog.FileName));
+                imagePreview.Source = _bitmapImage;
+                fileTextBox.Text = openDialog.FileName;
+                fileSizeLabel.Content = $"Rozmiar: {_bitmapImage.PixelWidth}px x {_bitmapImage.PixelHeight}px";
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var saveDialog = new SaveFileDialog();
+            saveDialog.Title = "Zapisz obraz jako";
+            saveDialog.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+            if (_bitmapImage != null)
+            {
+                if (saveDialog.ShowDialog() == true)
+                {
+                    var jpg = new JpegBitmapEncoder();
+                    jpg.Frames.Add(BitmapFrame.Create(_bitmapImage));
+                    using (var fileStream = File.Create(saveDialog.FileName))
+                    {
+                        jpg.Save(fileStream);
+                    }
+                }
+            }
         }
     }
 }
